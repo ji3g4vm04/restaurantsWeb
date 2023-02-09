1aa0dea78194ca398bb22cb6c080ba8675edcafd
LiXuan <ji3g4vm04@gmail.com>
2023-02-09 20:57:53 +0800

    路徑更新

--- dist/src/Routes/index.js
+++ dist/src/Routes/index.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 import restaurantRoute from './restaurantRoute.js';
 const router = Router();
 // 所有路徑將由restaurant 開始
-router.use('/restaurant', restaurantRoute);
+router.use('/', restaurantRoute);
 export default router;
 //# sourceMappingURL=index.js.map
